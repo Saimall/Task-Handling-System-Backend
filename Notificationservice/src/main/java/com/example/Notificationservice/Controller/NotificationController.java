e957e1d751c70d6a88290d6f72f754fa0f4414d7
Sai Mallik <rameshwaramsaimallik@gmail.com>
2024-11-02 22:46:53 +0530

    Added notificationservice

---
+++ Notificationservice/src/main/java/com/example/Notificationservice/Controller/NotificationController.java
@@ -0,0 +1,28 @@
+package com.example.Notificationservice.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Notificationservice.DTO.EmailrequestDto;
+import com.example.Notificationservice.EmailService.EmailService;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+	
+	
+	 @Autowired
+	    private EmailService emailService;
+
+	    @PostMapping("/sendEmail")
+	    public ResponseEntity<String> sendEmailNotification(@RequestBody EmailrequestDto emailRequest) {
+	        emailService.sendEmail(emailRequest.getToEmail(), emailRequest.getSubject(), emailRequest.getBody());
+	        return ResponseEntity.ok("Email sent successfully");
+	    }
+	
+
+}
---
+++ Notificationservice/src/main/java/com/example/Notificationservice/DTO/EmailrequestDto.java
@@ -0,0 +1,47 @@
+package com.example.Notificationservice.DTO;
+
+public class EmailrequestDto {
+	
+	private String toEmail;
+	private String subject;
+    private String body;
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "EmailrequestDto [toEmail=" + toEmail + ", subject=" + subject + ", body=" + body + "]";
+	}
+	
+	
+	public EmailrequestDto(String toEmail, String subject, String body) {
+		super();
+		this.toEmail = toEmail;
+		this.subject = subject;
+		this.body = body;
+	}
+	
+	
+	
+    public String getToEmail() {
+		return toEmail;
+	}
+	public void setToEmail(String toEmail) {
+		this.toEmail = toEmail;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+
+}
